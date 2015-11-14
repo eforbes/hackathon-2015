6178598ef7221556a665594b192af7c789e9d7f5
Will Smith <mink.cv@gmail.com>
2015-11-14 13:52:33 -0600

    index and downforthis pages setup

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET downforthis page. */
+router.get('/downforthis', function(req, res, next) {
+  res.render('downforthis', { title: 'Down for this?' });
+});
+
 module.exports = router;
