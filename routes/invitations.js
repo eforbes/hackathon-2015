503988fea19470e4baef4000cf3bf83b558c847a
Michael Ripley <zkxs00@gmail.com>
2015-11-14 16:58:01 -0600

    query to add new invitation to database

--- routes/invitations.js
+++ routes/invitations.js
@@ -1,16 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
 var common = require('./../common.js');
 
 router.post('/invite', ensureAuthenticated, function(req, res, next) {
   console.log("invite: ", JSON.stringify(req.body));
-
+  
 });
 
 
 function invite(eventId, userId, next) {
-
+  console.log("submit invitation: ", eventId, ", ", userId);
+  
+  common.pool.query("INSERT INTO `invitation` (user_id, event_id) VALUES (?,?)",
+    [userId, eventId],
+    function(err, rows, fields) {
+      if (err) {
+        return next(err);
+      }
+      else {
+        return next(null);
+      }
+    }
+  );
+  
 }
 
 
