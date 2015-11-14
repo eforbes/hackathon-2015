e644d116e341ac0510d5e486368616f2206af324
Michael Ripley <zkxs00@gmail.com>
2015-11-14 17:24:23 -0600

    handle invitation requests

--- routes/invitations.js
+++ routes/invitations.js
@@ -5,6 +5,52 @@ var common = require('./../common.js');
 router.post('/invite', ensureAuthenticated, function(req, res, next) {
   console.log("invite: ", JSON.stringify(req.body));
   
+  if (req.body.type === "user") {
+    invite(req.body.eventId, req.body.id,
+      function(err){
+        if (err) {
+          res.sendStatus(500);
+        }
+        else {
+          res.sendStatus(200);
+        }
+      }
+    );
+  }
+  else if (req.body.type === "group") {
+    
+    // get group members
+    common.pool.query("SELECT `user_id` FROM `group_membership` WHERE `group_id` = ?",
+      [req.body.id],
+      function(err, rows, fields){
+        if (err) {
+          return next(err);
+        }
+        
+        for (var i = 0; i < rows.length; i++) {
+          invite(req.body.eventID, rows[i],
+            function(err) {
+              // do nothing, dangerously
+              // evan knows a fancy way of doing this correctly
+            }
+          );
+        }
+      }
+    );
+    
+    // for each member
+    
+    invite(req.body.eventId, <PERSONID>,
+      function(err) {
+        
+      }
+    );
+  }
+  else {
+    // bad string
+    res.sendStatus(500);
+  }
+  
 });
 
 
