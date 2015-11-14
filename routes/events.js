290d4dd2364305b646448f8ead1297c95ab80ce1
Michael Ripley <zkxs00@gmail.com>
2015-11-14 16:38:24 -0600

    query to add event to database

--- routes/events.js
+++ routes/events.js
@@ -9,10 +9,21 @@ router.get('/', ensureAuthenticated, function(req, res, next) {
 });
 
 router.post('/', ensureAuthenticated, function(req, res, next) {
-  console.log("submit event: ", JSON.stringify(request.body));
-
-
-
+  console.log("submit event: ", JSON.stringify(req.body));
+  
+  common.pool.query("INSERT INTO `event` (owner, title, description, location, start_time, response_deadline, minimum_attendance) VALUES (?,?,?,?,?,?,?)",
+    [req.user.id, req.body.title, req.body.description, req.body.location, req.body.start_time, req.body.response_deadline, req.body.minimum_attendance],
+    function(err, rows, fields) {
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      else {
+        res.redirect('/events');
+        return;
+      }
+    }
+  );
   
 });
 
