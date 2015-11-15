5f3583f59623369b0030a6987193dab4d57af20c
Michael Ripley <zkxs00@gmail.com>
2015-11-14 18:50:38 -0600

    Send list of events user is invited to back to frontend

--- routes/events.js
+++ routes/events.js
@@ -3,10 +3,28 @@ var router = express.Router();
 
 var common = require('./../common.js');
 
-router.get('/', ensureAuthenticated, function(req, res, next) {
-  console.log("user", JSON.stringify(req.user));
-  res.render('downforthis', { title: 'Down for this?', user: req.user});
-});
+router.get('/', ensureAuthenticated,
+  function(req, res, next) {
+    console.log("user", JSON.stringify(req.user));
+    
+    common.pool.query(
+      "SELECT event_id, condition_id, `invitation`.status AS status, owner AS owner_id, title, description, location, start_time, response_deadline, minimum_attendance, `event`.`status` AS event_status, `owning_user`.name AS owner_name, `owning_user`.email AS owner_email, `owning_user`.image_url AS owner_image, ( SELECT COUNT(*) FROM `invitation` `i` WHERE `i`.event_id = `invitation`.event_id ) as number_invited, ( SELECT COUNT(*) FROM `invitation` `i` WHERE `i`.event_id = `invitation`.event_id AND `i`.`status` = 1 ) as number_attending FROM `invitation` LEFT JOIN `event` ON event_id = `event`.id LEFT JOIN `user` AS owning_user ON `owner` = owning_user.id WHERE user_id = ? and hidden = 0;",
+      [req.user.id],
+      function(err, rows, fields) {
+        if(err) {
+          res.sendStatus(500);
+          return;
+        }
+        
+        res.render('downforthis', {
+          title: 'Down for this?',
+          user: req.user,
+          events: rows
+        });
+      }
+    );
+  }
+);
 
 router.post('/', ensureAuthenticated, function(req, res, next) {
   console.log("submit event: ", JSON.stringify(req.body));
