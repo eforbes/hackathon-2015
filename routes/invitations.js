033555cdf2fa762b52873cfc22148a7c2c4f6c97
Michael Ripley <zkxs00@gmail.com>
2015-11-14 23:06:15 -0600

    add insecure invitations

--- routes/invitations.js
+++ routes/invitations.js
@@ -2,49 +2,100 @@ var express = require('express');
 var router = express.Router();
 var common = require('./../common.js');
 
-router.post('/invite', ensureAuthenticated, function(req, res, next) {
-  console.log("invite: ", JSON.stringify(req.body));
-  
-  if (req.body.type === "user") {
-    invite(req.body.eventId, req.body.id,
-      function(err){
-        if (err) {
-          res.sendStatus(500);
+
+// invite a user or group to an event
+router.post('/invite', ensureAuthenticated,
+  function(req, res, next) {
+    console.log("invite: ", JSON.stringify(req.body));
+    
+    if (req.body.type === "user") {
+      invite(req.body.eventId, req.body.id,
+        function(err){
+          if (err) {
+            res.sendStatus(500);
+          }
+          else {
+            res.sendStatus(200);
+          }
         }
-        else {
+      );
+    }
+    else if (req.body.type === "group") {
+      
+      // get group members
+      common.pool.query("SELECT `user_id` FROM `group_membership` WHERE `group_id` = ?",
+        [req.body.id],
+        function(err, rows, fields){
+          if (err) {
+            return next(err);
+          }
+          
+          for (var i = 0; i < rows.length; i++) {
+            invite(req.body.eventID, rows[i].user_id,
+              function(err) {
+                // do nothing, dangerously
+                // evan knows a fancy way of doing this correctly
+              }
+            );
+          }
           res.sendStatus(200);
         }
-      }
-    );
+      );
+    }
+    else {
+      // bad string
+      res.sendStatus(500);
+    }
   }
-  else if (req.body.type === "group") {
-    
-    // get group members
-    common.pool.query("SELECT `user_id` FROM `group_membership` WHERE `group_id` = ?",
-      [req.body.id],
-      function(err, rows, fields){
-        if (err) {
-          return next(err);
-        }
-        
-        for (var i = 0; i < rows.length; i++) {
-          invite(req.body.eventID, rows[i].user_id,
-            function(err) {
-              // do nothing, dangerously
-              // evan knows a fancy way of doing this correctly
-            }
-          );
-        }
-      }
-    );
+);
 
+
+// INSECURELY invite a user or group to an event
+router.post('/secureInvite',
+  function(req, res, next) {
+    console.log("invite: ", JSON.stringify(req.body));
+    
+    if (req.body.type === "user") {
+      invite(req.body.eventId, req.body.id,
+        function(err){
+          if (err) {
+            console.log(err);
+            res.sendStatus(500);
+          }
+          else {
+            res.sendStatus(200);
+          }
+        }
+      );
+    }
+    else if (req.body.type === "group") {
+      
+      // get group members
+      common.pool.query("SELECT `user_id` FROM `group_membership` WHERE `group_id` = ?",
+        [req.body.id],
+        function(err, rows, fields){
+          if (err) {
+            return next(err);
+          }
+          
+          for (var i = 0; i < rows.length; i++) {
+            invite(req.body.eventID, rows[i].user_id,
+              function(err) {
+                // do nothing, dangerously
+                // evan knows a fancy way of doing this correctly
+              }
+            );
+          }
+          res.sendStatus(200);
+        }
+      );
+    }
+    else {
+      // bad string
+      res.sendStatus(500);
+    }
   }
-  else {
-    // bad string
-    res.sendStatus(500);
-  }
-  
-});
+);
 
 
 function invite(eventId, userId, next) {
