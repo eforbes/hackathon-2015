5395fceb271185e97093cb646693f8ff2fcb2d63
Michael Ripley <zkxs00@gmail.com>
2015-11-15 00:40:00 -0600

    added insecure api to get list of favorite users

--- routes/users.js
+++ routes/users.js
@@ -33,7 +33,7 @@ router.get('/getUserByEmail', function(req, res, next){
 });
 
 // get all of my favorites
-router.get('/getFavoriteUsers', ensureAuthenticated
+router.get('/getFavoriteUsers', ensureAuthenticated,
   function(req, res, next) {
     common.pool.query("SELECT id, name, email, image_url FROM favorite LEFT JOIN `user` on favoritee = id WHERE favoriter = ?;",
       [req.user.id],
@@ -43,6 +43,22 @@ router.get('/getFavoriteUsers', ensureAuthenticated
           return;
         }
         res.send(rows);
+      }
+    );
+  }
+);
+
+// INSECURELY get all of my favorites
+router.post('/getFavoriteUsers',
+  function(req, res, next) {
+    common.pool.query("SELECT id, name, email, image_url FROM favorite LEFT JOIN `user` on favoritee = id WHERE favoriter = ?;",
+      [req.body.id],
+      function(err, rows, fields) {
+        if (err) {
+          res.sendStatus(500);
+          return;
+        }
+        res.send({rows: rows});
       }
     );
   }
