8a79e958edba2c55403a57f5d5ab20e51d8fd230
Michael Ripley <zkxs00@gmail.com>
2015-11-14 20:16:00 -0600

    can now post to setFavorite to set or unset favorite status on another user

--- routes/users.js
+++ routes/users.js
@@ -8,61 +8,93 @@ var common = require('./../common.js');
 
 router.get('/return', passport.authenticate('google', {failureRedirect: '/error'}),
  function(req, res){
-  	res.redirect('/events');
+    res.redirect('/events');
 });
 
 router.get('/', ensureNotAuthenticated, passport.authenticate('google'));
 
 router.get('/getUserByEmail', function(req, res, next){
-	console.log("get user by email", req.body);
-	var email = req.body.email;
-	common.pool.query("SELECT * FROM user WHERE email=?", [email], function(err, rows, fields){
-		if(err) {
-			res.sendStatus(500);
-			return;
-		}
-		if(rows.length==0) {
-			res.sendStatus(404);
-			return;
-		}
-		res.send(rows[0]);
-	});
+  console.log("get user by email", req.body);
+  var email = req.body.email;
+  common.pool.query("SELECT * FROM user WHERE email=?",
+    [email],
+    function(err, rows, fields) {
+      if(err) {
+        res.sendStatus(500);
+        return;
+      }
+      if(rows.length==0) {
+        res.sendStatus(404);
+        return;
+      }
+      res.send(rows[0]);
+    }
+  );
 });
 
+// set or unset a user as a favorite
+router.post('/setFavorite', ensureAuthenticated,
+  function(req, res, next) {
+    if (req.body.favorite) {
+      common.pool.query("INSERT IGNORE INTO favorite (favoriter, favoritee) VALUES (?, ?)",
+        [req.user.id, req.body.favoriteeId],
+        function(err, rows, fields) {
+          if (err) {
+            res.sendStatus(500);
+            return;
+          }
+          res.send({status: true});
+        }
+      );
+    }
+    else {
+      common.pool.query("DELETE FROM favorite WHERE favoriter = ? AND favoritee = ?",
+        [req.user.id, req.body.favoriteeId],
+        function(err, rows, fields) {
+          if (err) {
+            res.sendStatus(500);
+            return;
+          }
+          res.send({status: false});
+        }
+      );
+    }
+  }
+);
 
 passport.use(new GoogleStrategy({
-	clientID: '492634215704-u67fql0da7poc7jqcf01c11ljgovdphf.apps.googleusercontent.com',
-    	clientSecret: 'bgrmAWX98WT9uLLDtKD-WmNj',
-    	callbackURL: "http://127.0.0.1:3000/login/return",
-    	scope: ['profile','email']
+  clientID: '492634215704-u67fql0da7poc7jqcf01c11ljgovdphf.apps.googleusercontent.com',
+      clientSecret: 'bgrmAWX98WT9uLLDtKD-WmNj',
+      callbackURL: "http://127.0.0.1:3000/login/return",
+      scope: ['profile','email']
   },
   function(accessToken, refreshToken, profile, done) {
-  	console.log("tok ", accessToken);
+    console.log("tok ", accessToken);
   
-  	var user = {
-  		openid: profile.id,
-  		name: profile.displayName,
-  		email: profile.emails[0].value,
-  		img: profile.photos[0].value
-  	};
+    var user = {
+      openid: profile.id,
+      name: profile.displayName,
+      email: profile.emails[0].value,
+      img: profile.photos[0].value
+    };
 
-  	common.pool.query('SELECT * FROM user WHERE openid = ?',[user.openid], function(err, rows, fields){
-  		if(err)  return done(err);
-  		
-  		if(rows.length>0) {
-  			console.log("user already exists");
-  			user.id = rows[0].id;
-  			return done(null, user);
-  		} 
+    common.pool.query('SELECT * FROM user WHERE openid = ?',[user.openid], function(err, rows, fields){
+      if(err)  return done(err);
+      
+      if(rows.length>0) {
+        console.log("user already exists");
+        user.id = rows[0].id;
+        return done(null, user);
+      } 
 
-  		console.log("new user");
-  		common.pool.query("INSERT INTO user (name, email, openid, image_url) VALUES (?,?,?,?)",[user.name, user.email, user.openid, user.img], function(err2, rows, fields){
-  			if(err2) return done(err2);
+      console.log("new user");
+      common.pool.query("INSERT INTO user (name, email, openid, image_url) VALUES (?,?,?,?)",[user.name, user.email, user.openid, user.img], function(err2, rows, fields){
+        if(err2) return done(err2);
 
-  			user.id = rows.insertId;
-  			return done(null, user);
-  		});
-  	});
+        user.id = rows.insertId;
+        return done(null, user);
+      });
+    });
   }
 ));
 
