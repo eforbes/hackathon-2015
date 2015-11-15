043ec0b0313f83e3cb8401a6adb37e2451081eaa
Evan Forbes <forbes.evan@gmail.com>
2015-11-15 02:52:01 -0600

    fix michcomma

--- routes/events.js
+++ routes/events.js
@@ -191,7 +191,7 @@ router.post('/secureGetAttendingUsers', ensureAuthenticated,
     
     common.pool.query(
       "SELECT user_id, `status`, name, email, image_url, NOT (favoritee IS NULL) AS is_favorite FROM `invitation` LEFT JOIN `user` on user_id = id LEFT JOIN favorite on favoriter = ? AND favoritee = user_id WHERE event_id = ?",
-      [req,body.id, req.body.eventId],
+      [req.body.id, req.body.eventId],
       function(err, rows, fields) {
         if(err) {
           res.sendStatus(500);
